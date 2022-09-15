5e9a70d775551d37388118c7fdb2b997055d08ac
Alex Comeau <17982529+alexm622@users.noreply.github.com>
2022-09-15 14:12:11 -0400

    data saving and restoring

--- run.sh
+++ run.sh
@@ -17,12 +17,37 @@ on_exit(){
 
 	echo gzipping
 	gzip -fv data.tar
-	
 
 
 
 	kill -- $$
 }
+
+big_enough(){
+	size=$(du -s data | awk '{print "", $1}')
+	if [ $size -gt 150000 ]; then
+		echo "big enough"
+		return 1
+	else
+		return 0
+	fi
+}
+
+unpack(){
+	rm -r data
+	tar -xvf data.tar.gz
+}
+
+if [ -d "data" ]; then
+	echo "exists"
+	if big_enough; then
+		echo corrupt/broken, refreshing from gz file
+		unpack
+	fi
+else
+	echo "missing"
+	unpack
+fi
 
 echo "starting containers"
 trap on_exit SIGINT SIGTERM
